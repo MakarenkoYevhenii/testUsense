61661fb6bef36d6e6da87be53e38a0618c2bdae8
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-12-20 14:50:03 +0400

    changeAny. refactor code,

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,48 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.sass']
+  styleUrls: ['./body.component.sass'],
 })
 export class BodyComponent {
-  password="";
-  firstCube="";
-  secondCube="";
-  thirdCube="";
- changeFn(e:any) {
-   this.password = e.target.value;
- }
- 
- StrongPassword=()=>{
-   const strongRegex:any = /(?=.*\d)(?=.*[a-z])(?=.*[a-z])(?=.*[\\\.\*!@#$%^&*(_+=?/>.,<)]).{8,}$/
-   const mediumRedex:any = /(?=.*[A-Za-z])(?=.*\d)|(?=.*[\\\.\*!@#$%^&*(_+=?/>.,<)])/
-   if(this.password.length===0){
-     this.firstCube="notwork"
-     this.secondCube="notwork"
-     this.thirdCube="notwork"
-     return
-   }
-   if (strongRegex.test(this.password)) {
-     this.firstCube="passwordStrong"
-     this.secondCube="passwordStrong"
-     this.thirdCube="passwordStrong"
-     return 
-   }
-   if (this.password.length<=8&&this.password.length>=1) {
-     this.firstCube="passwordLess8Digits"
-     this.secondCube="passwordLess8Digits"
-     this.thirdCube="passwordLess8Digits"
-     return 
-   }
-   if (mediumRedex.test(this.password)) {
-     this.firstCube="mediumPassword"
-     this.secondCube="mediumPassword"
-     this.thirdCube="notwork"
-     return 
-   }
-   this.firstCube="passwordLess8Digits"
-     this.secondCube="notwork"
-     this.thirdCube="notwork"
+  password = '';
+  firstCube = 'notwork';
+  secondCube = 'notwork';
+  thirdCube = 'notwork';
 
-   return 
- }
+  strongRegex: RegExp =
+    /(?=.*\d)(?=.*[a-z])(?=.*[a-z])(?=.*[\\\.\*!@#$%^&*(_+=?/>.,<)]).{8,}$/;
+  mediumRedex: RegExp =
+    /(?=.*[A-Za-z])(?=.*\d).{8,}$|(?=.*[\\\.\*!@#$%^&*(_+=?/>.,<)]).{8,}$/;
+
+  ChangeFn(e: Event) {
+    this.password = (e.target as HTMLInputElement).value;
+  }
+
+  StrongPassword = () => {
+    // if (this.password.length <= 8 && this.password.length >= 1) {
+    //   this.firstCube = 'passwordLess8Digits';
+    //   this.secondCube = 'passwordLess8Digits';
+    //   this.thirdCube = 'passwordLess8Digits';
+    //   return;
+    // }
+    if (this.strongRegex.test(this.password)) {
+      this.firstCube = 'passwordStrong';
+      this.secondCube = 'passwordStrong';
+      this.thirdCube = 'passwordStrong';
+      return;
+    }
+
+    if (this.mediumRedex.test(this.password)) {
+      this.firstCube = 'mediumPassword';
+      this.secondCube = 'mediumPassword';
+      this.thirdCube = 'notwork';
+      return;
+    }
+    this.firstCube =
+      this.password.length === 0 ? 'notwork' : 'passwordLess8Digits';
+    this.secondCube =
+      this.password.length <= 8 && this.password.length >= 1
+        ? 'passwordLess8Digits'
+        : 'notwork';
+    this.thirdCube =
+      this.password.length <= 8 && this.password.length >= 1
+        ? 'passwordLess8Digits'
+        : 'notwork';
+    return;
+  };
 }
